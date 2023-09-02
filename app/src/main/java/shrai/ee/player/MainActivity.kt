d402b50271af10a160949b2270b89ab0e1cf6cdf
Shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2023-09-02 16:41:44 +0530

    AndroidApp

--- app/src/main/java/shrai/ee/player/MainActivity.kt
+++ app/src/main/java/shrai/ee/player/MainActivity.kt
@@ -2,10 +2,26 @@ package shrai.ee.player
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.note_layout)
+        val cancel:Button? = findViewById(R.id.button)
+        cancel?.setOnClickListener {
+            finish()
+        }
+        val ok:Button? = findViewById(R.id.button3)
+        ok?.setOnClickListener {
+            setContentView(R.layout.activity_main)
+            val back:Button? = findViewById(R.id.button2)
+            back?.setOnClickListener {
+                setContentView(R.layout.note_layout)
+            }
+        }
+
     }
 }
